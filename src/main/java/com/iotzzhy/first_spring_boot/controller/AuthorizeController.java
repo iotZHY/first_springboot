7b6b8eb8e4f2a6a6deaad8a065392cbc15530c1c
CodeMakerYY <45481949+CodeMakerYY@users.noreply.github.com>
2020-03-23 22:38:06 +0800

    利用配置文件抽离变量

--- src/main/java/com/iotzzhy/first_spring_boot/controller/AuthorizeController.java
+++ src/main/java/com/iotzzhy/first_spring_boot/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.iotzzhy.first_spring_boot.dto.AccessTokenDTO;
 import com.iotzzhy.first_spring_boot.dto.GithubUser;
 import com.iotzzhy.first_spring_boot.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.uri}")
+    private String clientUri;
+
     @GetMapping("callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("9b5498ade1e9c7b97078");
-        accessTokenDTO.setClient_secret("0bbf52ef27722d26d08832f7f26a8d28fa9b5a09");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(clientUri);
         accessTokenDTO.setState(state);
         String token = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(token);
